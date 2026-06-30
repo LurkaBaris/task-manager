f990640c7d9c1dac3afe642e5c6b1b0ed513a3e5
Latnove <r1ghtnowcp2@gmail.com>
2026-06-30 16:40:55 +0300

    fix: configure project setup

--- eslint.config.js
+++ eslint.config.js
@@ -1,4 +1,6 @@
 import js from '@eslint/js'
+import reactHooks from 'eslint-plugin-react-hooks'
+import reactRefresh from 'eslint-plugin-react-refresh'
 import { defineConfig } from 'eslint/config'
 import globals from 'globals'
 import tseslint from 'typescript-eslint'
@@ -17,8 +19,16 @@ export default defineConfig([
     languageOptions: {
       globals: globals.browser,
     },
+    plugins: {
+      'react-hooks': reactHooks,
+      'react-refresh': reactRefresh,
+    },
     rules: {
+      ...reactHooks.configs.recommended.rules,
+
       'no-empty': ['error', { allowEmptyCatch: true }],
+
+      'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
     },
   },
 
